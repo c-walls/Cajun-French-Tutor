275b661a67808efb43bc1170eb144ff909a90d3b
Caleb Walls <calebmarcuswalls@gmail.com>
2026-02-08 14:50:21 -0600

    add multiturn convo

--- backend/app.py
+++ backend/app.py
@@ -6,15 +6,19 @@ from pathlib import Path
 from google import genai
 
 app = FastAPI(title="Cajun French Tutor")
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client()
 
 # Locate static directory: ../frontend/static
 STATIC_DIR = Path(__file__).resolve().parents[1] / "frontend" / "static"
 app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 
+# Setup gemini chat session for testing
 chat_history = []
-print(f"DEBUG: Gemini Key is: {api_key}")
+client = genai.Client()
+chat = client.chats.create(
+    model="gemini-2.5-flash",
+    config={'system_instruction': "You are a helpful Cajun French tutor."}
+)
+
 
 @app.get("/")
 async def root():
@@ -26,16 +30,11 @@ async def chat(message: str = Form(...)):
     chat_history.append({"role": "user", "content": message})
 
     # Gemini used as a standin for testing
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=message,
-        config={'system_instruction': "You are a helpful Cajun French tutor."}
-    )
-    
+    response = chat.send_message(message)
     bot_response = response.text
     chat_history.append({"role": "bot", "content": bot_response})
     
-    # 4. Return pre-rendered HTML fragments
+    # Return pre-rendered HTML fragments
     return f"""
         <article class="bubble user">{message}</article>
         <article class="bubble bot">{bot_response}</article>
